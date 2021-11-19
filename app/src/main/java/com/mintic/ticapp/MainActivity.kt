2a3552863ff7574b66117aac8bb9afa339ead3f8
Anderson Morales <amoralesch@unal.edu.co>
2021-11-18 23:56:57 -0500

    El contenido lo volví fragments. Solo quedó una activity que contiene los botones. Además, incluyo un archivo JSON con algunos lugares de México con sus respectivas imágenes.

--- app/src/main/java/com/mintic/ticapp/MainActivity.kt
+++ app/src/main/java/com/mintic/ticapp/MainActivity.kt
@@ -2,10 +2,47 @@ package com.mintic.ticapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager =  supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val btnPoi = findViewById<ImageButton>(R.id.btnPoi)
+        val btnInfo = findViewById<ImageButton>(R.id.btnInfoLugar)
+
+        val fragment = Lugar_fragment()
+        fragmentTransaction.add(R.id.main_container, fragment)
+        fragmentTransaction.commit()
+
+        btnPoi.setOnClickListener {
+            clickBtnPoi()
+        }
+
+        btnInfo.setOnClickListener {
+            clickBtnInfo()
+        }
+    }
+
+    private fun clickBtnPoi(){
+        val transaction = supportFragmentManager.beginTransaction()
+        val newFragment = Poi_fragment()
+        transaction.replace(R.id.main_container, newFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    private fun clickBtnInfo(){
+        val transaction = supportFragmentManager.beginTransaction()
+        val newFragment = Lugar_fragment()
+        transaction.replace(R.id.main_container, newFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
